fdf7a4a94f589e3f1e509ebb932a94b83e2f22af
cwx727 <cwx727@163.com>
2019-08-01 15:30:14 +0800

    third commit

--- util/send_email.py
+++ util/send_email.py
@@ -7,8 +7,8 @@ class SendEmail:
 	global email_host
 	global password
 	email_host = "smtp.163.com"
-	send_user = 'cwx727@163.com'
-	password = "a123456"
+	send_user = 'XXXX@163.com'
+	password = "ppppp"
 	def send_mail(self,user_list,sub,content):
 		user = "cwx727" + "<"+ send_user+ ">"   #send_user发件人邮箱地址 
 		message = MIMEText(content,_subtype='plain',_charset='utf-8')  #content邮件内容
@@ -32,10 +32,10 @@ class SendEmail:
 		fail_result = "%.2f%%" %(float(fail_num)/float(total_num)*100)
 		content = '本次测试通过'+str(pass_num)+'个,失败'+str(fail_num)+'个，通过率'+pass_result+',失败率'+fail_result
 		sub = '接口测试报告'
-		user_list=['2846634343@qq.com']
+		user_list=['ZZZZ@qq.com']
 		self.send_mail(user_list,sub,content)
 
 
 if __name__ == '__main__':
-	#user_list=['2846634343@qq.com']
+	#user_list=['ZZZZ@qq.com']
 	SendEmail().send_main([1,3,5,7],[2,4])
